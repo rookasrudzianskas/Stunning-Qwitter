71c7a1ebe5c81d2581a72f9dc5fba8d69c93be7f
Rokas <rookas.rudzenskas@gmail.com>
2021-04-14 14:22:49 +0300

    Rokas

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,11 +1,65 @@
 <template>
   <q-page>
-<h5>Home</h5>
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+      <div class="col">
+        <q-input
+          bottom-slots
+          v-model="newQweetContent"
+          class="new-qweet"
+          placeholder="What's happening?"
+          counter
+          maxlength="280"
+          :dense="dense"
+          autogrow
+        >
+          <template v-slot:before>
+            <q-avatar size="xl">
+              <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+            </q-avatar>
+          </template>
+        </q-input>
+      </div>
+      <div class="col col-shrink">
+        <q-btn
+          class="q-mb-lg"
+          :disable="!newQweetContent"
+          unelevated
+          rounded color="primary"
+          label="QTweet"
+          no-caps
+        />
+      </div>
+
+    </div>
+    <q-separator
+      size="10px"
+      color="grey-2"
+      class="divider"
+    />
+
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageHome'
+  name: 'PageHome',
+  data() {
+    return {
+      newQweetContent: ''
+    }
+  }
+
 }
 </script>
+
+<style lang="sass">
+.new-qweet
+  textarea
+    font-size: 19px
+    line-height: 1.4 !important
+
+.divider
+  border-top: 1px solid
+  border-bottom: 1px solid
+  border-color: $grey-4
+</style>
