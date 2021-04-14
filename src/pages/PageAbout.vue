3027208d9d50010314424c6d2dfda1f19aea05e4
Rokas <rookas.rudzenskas@gmail.com>
2021-04-14 14:00:09 +0300

    Rokas

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,6 +1,22 @@
 <template>
-  <q-page>
-<h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+      <div class="text-body1">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, voluptatem!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, voluptatem!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, voluptatem!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, voluptatem!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, voluptatem!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, voluptatem!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, voluptatem!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, voluptatem!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, voluptatem!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, voluptatem!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, voluptatem!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, voluptatem!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, voluptatem!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, voluptatem!</p>
+          </div>
   </q-page>
 </template>
 
