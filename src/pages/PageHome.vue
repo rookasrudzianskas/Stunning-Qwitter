5c2f46e2ccf3272f7acda0dca4f1f38941926ef3
Rokas <rookas.rudzenskas@gmail.com>
2021-04-14 14:30:22 +0300

    Rokas

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -37,6 +37,32 @@
       class="divider"
     />
 
+    <q-list>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://twitter.com/byrookas/photo">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label ><strong>Rookas Rudzianskas</strong>
+          <span class="text-grey-7">
+            @rookasrudzianskas
+          </span>
+          </q-item-label>
+          <q-item-label>
+            <span class="text-weight-bold">Janet</span>
+            -- I'll be in your neighborhood doing errands this
+            weekend. Do you want to grab brunch?
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
